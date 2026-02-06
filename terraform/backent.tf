31a4a9c55216ce4a54cdd41b37454da686b41097
Ashgan Adel <adelashgan9@gmail.com>
2026-02-06 11:38:37 +0200

    Change Terraform backend region from us-east-1 to us-east-2

--- terraform/backent.tf
+++ terraform/backent.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "s3test128"
     key    = "state/file.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "lock_table" 
     encrypt = true
   }
